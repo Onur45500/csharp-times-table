591bbe2a82947f14b3d683453aa8b0b73c96156b
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 11:11:53 +0200

    Implemented the functionnality

--- csharp-times-table/Program.cs
+++ csharp-times-table/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Write a number to multiply until 10 :");
+            string stringNumber = Console.ReadLine();
+
+            int intNumber = Convert.ToInt32(stringNumber);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", i, intNumber, i * intNumber);
+            }
         }
     }
     
